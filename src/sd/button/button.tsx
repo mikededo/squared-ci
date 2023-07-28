9d1d996fdd718667b8b16d5604487ac57798af16
Miquel de Domingo <miquelddg@gmail.com>
2023-07-28 16:01:48 +0200

    refactor: button variants

--- src/sd/button/button.tsx
+++ src/sd/button/button.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames';
 import React from 'react';
 
+type Variant = 'primary' | 'secondary' | 'text';
 type DefaultProps = Omit<
   React.DetailedHTMLProps<
     React.ButtonHTMLAttributes<HTMLButtonElement>,
@@ -9,10 +10,18 @@ type DefaultProps = Omit<
   'children'
 >;
 type InternalProps = {
-  variant?: 'primary' | 'secondary' | 'text';
+  variant?: Variant;
   children: string;
 };
 type Props = DefaultProps & InternalProps;
+
+const Variants: Record<Variant, string> = {
+  primary:
+    'bg-indigo-500 hover:bg-indigo-400 active:bg-indigo-600 dark:bg-indigo-600 dark:hover:bg-indigo-500 dark:active:bg-indigo-400',
+  secondary:
+    'bg-amber-500 hover:bg-amber-400 active:bg-amber-600 dark:bg-slate-700 dark:hover:bg-slate-600 dark:active:bg-slate-500',
+  text: 'bg-transparent hover:bg-slate-100 active:bg-slate-200 dark:hover:bg-slate-700 dark:active:bg-slate-600 dark:bg-opacity-50',
+};
 
 export const Button: React.FC<Props> = ({
   className,
@@ -23,26 +32,18 @@ export const Button: React.FC<Props> = ({
   <button
     className={classNames(
       'py-2 px-4 rounded-full transition-colors',
-      variant === 'primary'
-        ? 'bg-indigo-500 hover:bg-indigo-400 active:bg-indigo-600 dark:bg-indigo-600 dark:hover:bg-indigo-500 dark:active:bg-indigo-400'
-        : '',
-      variant === 'secondary'
-        ? 'bg-amber-500 hover:bg-amber-400 active:bg-amber-600 dark:bg-slate-700 dark:hover:bg-slate-600 dark:active:bg-slate-500'
-        : '',
-      variant === 'text'
-        ? 'bg-transparent hover:bg-slate-100 active:bg-slate-200 dark:hover:bg-slate-700 dark:active:bg-slate-600 dark:bg-opacity-50'
-        : '',
+      Variants[variant],
       className
     )}
     {...props}
   >
-    <p
+    <span
       className={classNames(
         'uppercase text-white text-sm font-semibold',
         variant === 'text' ? 'text-current dark:text-white' : ''
       )}
     >
       {children}
-    </p>
+    </span>
   </button>
 );
