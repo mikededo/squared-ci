f3c5bc954e47a06b384a162f0de1889ceb05039e
Miquel de Domingo <miquelddg@gmail.com>
2023-08-08 18:44:20 +0200

    feat: add transition into news dialog

--- src/components/news/news.tsx
+++ src/components/news/news.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Button, VCol } from '@/sd';
+import { AppearTransition, Button, VCol } from '@/sd';
 
 import { Dialog } from './dialog';
 import type { NewItem } from './new';
@@ -84,36 +84,38 @@ const NewsList: NewItem[] = [
 export const News: React.FC = () => {
   const { opened, onToggleStatus, onHideNews } = useNews();
 
-  return !opened ? null : (
-    <Dialog>
-      <VCol variant="xl" className=" h-full w-full">
-        <h2 className="text-2xl font-semibold p-5 pb-0">News</h2>
-        <div className="self-start gap-4 p-2 rounded-lg text-center mx-5 bg-slate-200 dark:bg-slate-700 text-sm">
-          <strong className="font-semibold inline-block">
-            Coming soon:&nbsp;
-          </strong>
-          <span>
-            coping the generated code to the clipboard, improved drag
-            experience, fixed global drag and more! Thanks for coming!
-          </span>
-        </div>
-        <div className="self-start flex-1 flex flex-col gap-4 px-5 w-full overflow-y-auto">
-          {NewsList.map(({ title, description, date }) => (
-            <New
-              key={title}
-              title={title}
-              description={description}
-              date={date}
-            />
-          ))}
-        </div>
-        <div className="flex justify-end p-5 pt-0 gap-2">
-          <Button onClick={onHideNews} variant="text">
-            Don&apos;t show again
-          </Button>
-          <Button onClick={onToggleStatus}>Understood</Button>
-        </div>
-      </VCol>
-    </Dialog>
+  return (
+    <AppearTransition show={opened}>
+      <Dialog>
+        <VCol variant="xl" className=" h-full w-full">
+          <h2 className="text-2xl font-semibold p-5 pb-0">News</h2>
+          <div className="self-start gap-4 p-2 rounded-lg text-center mx-5 bg-slate-200 dark:bg-slate-700 text-sm">
+            <strong className="font-semibold inline-block">
+              Coming soon:&nbsp;
+            </strong>
+            <span>
+              coping the generated code to the clipboard, improved drag
+              experience, fixed global drag and more! Thanks for coming!
+            </span>
+          </div>
+          <div className="self-start flex-1 flex flex-col gap-4 px-5 w-full overflow-y-auto">
+            {NewsList.map(({ title, description, date }) => (
+              <New
+                key={title}
+                title={title}
+                description={description}
+                date={date}
+              />
+            ))}
+          </div>
+          <div className="flex justify-end p-5 pt-0 gap-2">
+            <Button onClick={onHideNews} variant="text">
+              Don&apos;t show again
+            </Button>
+            <Button onClick={onToggleStatus}>Understood</Button>
+          </div>
+        </VCol>
+      </Dialog>
+    </AppearTransition>
   );
 };
